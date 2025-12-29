58a223beedd2d5a5d12aeb1713e2a2b9f1ebf12c
1i2s3s4a5k6a <109131553+1i2s3s4a5k6a@users.noreply.github.com>
2025-12-29 13:39:23 +0000

    Handle response and schedule expiry alert
    Added logic to handle response and schedule expiry alert based on expiry date.

--- main.dart
+++ main.dart
@@ -38,6 +38,25 @@ class _FreshTrackAppState extends State<FreshTrackApp> {
     }
   }
 
+  if (response.statusCode == 200) {
+  var data = json.decode(response.body);
+  String foundDateStr = data['expiry_date'];
+
+  if (foundDateStr != "No date detected") {
+    // Convert string "MM/DD/YYYY" to DateTime object
+    DateTime expiry = DateTime.parse(foundDateStr.replaceAll('/', '-')); 
+    
+    // Save to database AND schedule alert
+    await scheduleExpiryAlert(DateTime.now().millisecondsSinceEpoch % 1000, data['food_item'], expiry);
+  }
+
+  setState(() {
+    _result = "Status: ${data['status']}\nExpiry: $foundDateStr";
+    _loading = false;
+  });
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
